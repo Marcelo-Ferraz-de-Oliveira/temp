d236c10b7c1a07956577e320068d8477291d2e33
marcelo <marcelo@marcelo-System-Product-Name>
2018-07-06 12:20:31 -0300

    criação do target e variáveis delta no teste_pandas

--- wall_e/archive/teste_pandas.py
+++ wall_e/archive/teste_pandas.py
@@ -10,15 +10,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime
 
-
-
-
-
-
-
-dia_inicial = 0
-dia_final = 5
-
 def Str_to_sec(data):
     data_str = str(data)
     if 'na' in data_str: return 0
@@ -50,6 +41,17 @@ def lista_arquivos(pasta):
             filtrada.append([parte2,item])
     
     return filtrada
+
+
+
+
+
+dia_inicial = 0
+dia_final = 5
+
+thr_target = 3
+
+n_segundos = 60
 
 dir_base = "/home/marcelo/Dados/Dados_Bolsa_Wall_e/Resultados/"
 lista_dir = lista_arquivos(dir_base)
@@ -116,22 +118,15 @@ for database in databases:
     compradores = list(dict(df.comprador.value_counts()).keys())
     vendedores = list(dict(df.vendedor.value_counts()).keys())
     corretoras = set().union(compradores,vendedores)
-    df['vol_real'] = np.where(df['direcao']=='V',df['Volume'].astype(int),np.where(df['direcao']=='A',df['Volume'].astype(int),0))
-    print(corretoras)
-    print(df.head())
-    input("")
+    df['vol_real2'] = np.where(df['direcao']=='V',df['Volume'].astype(int),np.where(df['direcao']=='A',df['Volume'].astype(int),0))
     for corretora in corretoras:
-        df[str(corretora)+"c"] = np.where(df['comprador'].astype(int)==int(corretora), df['vol_real'],0)
-        df[str(corretora)+"v"] = np.where(df['vendedor'].astype(int)==int(corretora), df['vol_real'].apply(lambda x: x*(-1)),0)
+        df[str(corretora)+"c"] = np.where(df['comprador'].astype(int)==int(corretora), df['vol_real2'],0)
+        df[str(corretora)+"v"] = np.where(df['vendedor'].astype(int)==int(corretora), df['vol_real2'].apply(lambda x: x*(-1)),0)
         df[str(corretora)+"c"] = df[str(corretora)+"c"].cumsum()
         df[str(corretora)+"v"] = df[str(corretora)+"v"].cumsum()
         df[str(corretora)] = df[str(corretora)+"c"] + df[str(corretora)+"v"]
-        df.drop([str(corretora)+"c",str(corretora)+"v"],axis=1,inplace=True)
-        #print(df[str(corretora)])
-    #df = df.fillna(method="ffill")
-    print(df.head())
-    print(df.tail())
-    input("")
+        #df.drop([str(corretora)+"c",str(corretora)+"v"],axis=1,inplace=True)
+    #print(df.tail())
     #Calcula o acumulado da agressão:
     #print(pd.to_datetime(df['Tempo']))
     #input("")
@@ -150,12 +145,68 @@ for database in databases:
     df['Preço'] = df['Preço'].astype(float)
     correlato = df[['Preço','acumulado']]
     print("Dia:",database[3])
-    print("Correlação:",df.corr(method='spearman').Preço['acumulado'])
-    dfs = df[['Tempo','Preço','acumulado','acumulado_positivo','acumulado_negativo']].drop_duplicates(subset='Tempo',keep='last')
+#    print("Correlação:",df.corr(method='spearman').Preço['acumulado'])
+    temp_corr = []
+    df['vol_real2'] = df['vol_real2'].cumsum()
+    df['vol_positivo2'] = df['vol_positivo'].cumsum()
+    df['vol_negativo2'] = df['vol_negativo'].cumsum()
+    for corretora in corretoras:
+        temp_corr.append((corretora, 
+                          df[['Preço',str(corretora)]].corr(method='spearman').Preço[str(corretora)], 
+                          df[str(corretora)].iloc[-1],
+                          df[str(corretora)+"c"].iloc[-1],
+                          df[str(corretora)+"v"].iloc[-1],
+                          df[str(corretora)+"c"].iloc[-1] / df['vol_positivo2'].iloc[-1]*100,
+                          df[str(corretora)+"v"].iloc[-1] / df['vol_negativo2'].iloc[-1]*100,
+#                          df['vol_positivo2'].iloc[-1],
+#                          df['vol_negativo2'].iloc[-1]
+                         ))
+    cor_corr = sorted(temp_corr, key=lambda x:x[2], reverse=True)
+    #for item in cor_corr:
+        #print(item)
+    top_cors_temp = cor_corr[:3]+cor_corr[-3:]
+    top_cors = []
+    for cor in top_cors_temp:
+        top_cors.append(cor[0])
+    print(top_cors)
+    #input("")
+    dfs = df[['Tempo',
+              'Preço',
+              'acumulado',
+              'acumulado_positivo',
+              'acumulado_negativo',
+              str(top_cors[0]),
+              str(top_cors[1]),
+              str(top_cors[2]),
+              str(top_cors[3]),
+              str(top_cors[4]),
+              str(top_cors[5]),
+             ]].drop_duplicates(subset='Tempo',keep='last')
     dfs.index = pd.DatetimeIndex(dfs.Tempo)
     dfs = dfs.reindex(range_tempo,fill_value=None)
     dfs.Tempo = dfs.index
     dfs = dfs.fillna(method="ffill")
+    dfs = dfs.resample(str(n_segundos)+"S").last()#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    #for item in top_cors:
+    dfs["Preço"] = dfs["Preço"].astype(float)
+    dfs["preço_target"] = np.where(dfs["Preço"]-dfs["Preço"].shift(-1) >= 0.01*thr_target, 
+                                   1, 
+                                   np.where(dfs["Preço"]-dfs["Preço"].shift(-1) <= -0.01*thr_target,
+                                            -1,
+                                            0)
+                                           )    
+    dfs['agr_1p'] = dfs['acumulado'] - dfs['acumulado'].shift(1)
+    dfs['agr_5p'] = dfs['acumulado'] - dfs['acumulado'].shift(5)
+    dfs['agr_10p'] = dfs['acumulado'] - dfs['acumulado'].shift(10)
+    dfs['agr_20p'] = dfs['acumulado'] - dfs['acumulado'].shift(20)
+    dfs['agr_50p'] = dfs['acumulado'] - dfs['acumulado'].shift(50)
+
+    dfs['preço_1p'] = dfs['Preço'] - dfs['Preço'].shift(1)
+    dfs['preço_5p'] = dfs['Preço'] - dfs['Preço'].shift(5)
+    dfs['preço_10p'] = dfs['Preço'] - dfs['Preço'].shift(10)
+    dfs['preço_20p'] = dfs['Preço'] - dfs['Preço'].shift(20)
+    dfs['preço_50p'] = dfs['Preço'] - dfs['Preço'].shift(50)
+    #print(dfs)
     #print(dfs[:30])
     #input("")
     dfs['vol_negativo']=dfs['acumulado_negativo']-dfs['acumulado_negativo'].shift(1)
@@ -180,10 +231,31 @@ for database in databases:
     dfs['escore_acm'] = np.where(dfs['escore_acm'] >=5,1,0)
     esim = True
     if esim:
-        dfs['vol_positivo'].apply(lambda row:20*row).plot()
-        dfs['vol_negativo'].apply(lambda row:20*row).plot()
-        dfs['acumulado'].apply(lambda row:row).plot()
-        dfs.Preço.apply(lambda row:(row-abertura)*10000000).plot()
+        #dfs['vol_positivo'].apply(lambda row:20*row).plot()
+        #dfs['vol_negativo'].apply(lambda row:20*row).plot()
+        dfs.Preço = dfs.Preço.apply(lambda row:(row-abertura)*10000000)
+        dfs.preço_target = dfs.preço_target.apply(lambda row:row*5000000)
+        dfs.agr_1p = dfs.agr_1p.apply(lambda row:row*10)
+        dfs.agr_5p = dfs.agr_5p.apply(lambda row:row*10)
+        dfs.agr_10p = dfs.agr_10p.apply(lambda row:row*10)
+        dfs.agr_20p = dfs.agr_20p.apply(lambda row:row*10)
+        dfs.agr_50p = dfs.agr_50p.apply(lambda row:row*10)
+        dfs.preço_1p = dfs.preço_1p.apply(lambda row:row*100000000)
+        dfs.preço_5p = dfs.preço_5p.apply(lambda row:row*100000000)
+        dfs.preço_10p = dfs.preço_10p.apply(lambda row:row*100000000)
+        dfs.preço_20p = dfs.preço_20p.apply(lambda row:row*100000000)
+        dfs.preço_50p = dfs.preço_50p.apply(lambda row:row*100000000)
+        #dfs['acumulado'].plot()
+        #dfs[str(top_cors[0])].plot()
+        #dfs[str(top_cors[1])].plot()
+        #dfs[str(top_cors[2])].plot()
+        #dfs[str(top_cors[3])].plot()
+        #dfs[str(top_cors[4])].plot()
+        #dfs[str(top_cors[5])].plot()
+        dfs.Preço.plot()
+        dfs.preço_1p.plot()
+        dfs.preço_target.plot()
+        dfs.agr_1p.plot()
         #dfs['vol_positivo'].apply(np.log).plot()
         #dfs['vol_negativo'].apply(np.log).plot()
         #dfs['acumulado'].apply(np.log).plot()
@@ -192,16 +264,12 @@ for database in databases:
         #dfs['escore'].apply(lambda row:row*5000000).plot()
         #dfs['escore_acm'].apply(lambda row:row*2000000).plot()
         plt.grid(True)
+        #plt.figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
         plt.show()
-        dfs.to_csv(novo_arquivo,sep=';')
-        #input("")
+        #dfs.to_csv(novo_arquivo,sep=';')
+        input("")
     
     #break
     #dfs['acumulado'].plot()
     
     #break
-
-
-
-
-
